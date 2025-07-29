aee8e86cd6853a4afda28604c4f9eea5837bce09
ni3rav <mahetan35@gmail.com>
2025-07-29 14:00:56 +0530

    init shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="h-screen w-screen grid place-items-center">
+      <Button>Heyaa</Button>
+    </div>
+  );
 }
 
 export default App;
