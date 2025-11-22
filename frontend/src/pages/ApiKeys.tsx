ec549e93af58d8b2dbba0b60e769d752991ae8ab
supalvasani <supalvasani@gmail.com>
2025-11-22 11:43:48 +0530

    [frontend]:fix apikeys

--- frontend/src/pages/ApiKeys.tsx
+++ frontend/src/pages/ApiKeys.tsx
@@ -8,7 +8,7 @@ export function ApiKeysPage() {
         <div>
           <h1 className='text-2xl font-bold tracking-tight'>API Keys</h1>
           <p className='text-muted-foreground text-sm'>
-            Manage your API keys for accessing the API.
+            Manage your API keys to access the Content
           </p>
         </div>
         <Button>
