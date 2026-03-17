34372a51af52d0ab0da8d346af9a8de0b74f63d1
ni3rav <mahetan35@gmail.com>
2026-03-17 23:08:16 +0530

    feat: run demo build

--- sdk/tsdown.config.ts
+++ sdk/tsdown.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  entry: "src/index.ts",
+  format: ["esm", "cjs"],
+  platform: "node",
+  target: "node20",
   dts: true,
+  sourcemap: true,
   exports: true,
-  // ...config options
-})
+  clean: true,
+  deps: {
+    skipNodeModulesBundle: true,
+  },
+});
